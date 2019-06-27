2bddd3cb9336225606647fe19ad72e73e07b82a2
Nora Pete <norapete19@gmail.com>
2019-06-27 16:25:54 +0200

    workshop-d4: multiplication-table comp.

---
+++ week-01/day-04/multiplication-table.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+let number: number = 15;
+let number2: number = 1
+
+while (number2 < 11) {
+    console.log(number2 + ' * ' + number + ' = ' + number * number2);
+    number2 ++;
+}
