0886e071bfe1d1cdaf9e62bd8f7fbfabfbdfa203
Nora Pete <norapete19@gmail.com>
2019-06-27 12:24:01 +0200

    workshop-d4: types

---
+++ week-01/day-04/types.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log('Nora Pete');
+console.log(25);
+console.log(1.68);
+
+console.log(22 + 13);
+console.log(22 - 13);
+console.log(22 * 13);
+console.log(22 / 13);
+console.log(22 % 13);
+
+console.log(17 * 5 * 6);
+console.log(((17 * 5 * 6) / (52 * 17)) * 100);
