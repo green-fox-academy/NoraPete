dbd3d9018631c216e0305a4ad2e92d7367297015
Nora Pete <norapete19@gmail.com>
2019-06-27 11:56:32 +0200

    workshop-d4: printing to the terminal

---
+++ week-01/day-04/printingToTheTerminalWindow.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log('Hello World!');
+
+console.log('Hello Nora!');
+
+console.log('Humpty Dumpty sat on a wall,');
+console.log('Humpty Dumpty had a great fall.');
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put Humpty together again.');
+
+console.log('Hello Viktor!');
+console.log('Hello Szabolcs!');
+console.log('Hello Sanyi!');
