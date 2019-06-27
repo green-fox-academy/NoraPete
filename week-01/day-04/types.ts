e6c3377f4d66cdf8262e1f4a3c18971962bb4074
Nora Pete <norapete19@gmail.com>
2019-06-27 12:53:43 +0200

    workshoh-d4: types correction, variables

--- week-01/day-04/types.ts
+++ week-01/day-04/types.ts
@@ -1,8 +1,12 @@
 'use strict';
 
+// introduce-yourself
+
 console.log('Nora Pete');
 console.log(25);
 console.log(1.68);
+
+//two-numbers
 
 console.log(22 + 13);
 console.log(22 - 13);
@@ -10,5 +14,12 @@ console.log(22 * 13);
 console.log(22 / 13);
 console.log(22 % 13);
 
-console.log(17 * 5 * 6);
-console.log(((17 * 5 * 6) / (52 * 17)) * 100);
+// coding-hours
+
+let codingHoursInADay: number = 6;
+let workingDaysInAWeek: number = 5;
+let weeksInASemester: number = 17;
+let averageWorkingHoursInAWeek: number = 52;
+
+console.log( codingHoursInADay * workingDaysInAWeek * weeksInASemester);
+console.log(((codingHoursInADay * workingDaysInAWeek * weeksInASemester) / (averageWorkingHoursInAWeek * weeksInASemester)) * 100);
