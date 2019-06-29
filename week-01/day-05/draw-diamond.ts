8428860e6e672a27088ff4e8eeb3ec0a6770bc01
Nora Pete <norapete19@gmail.com>
2019-06-29 22:56:14 +0200

    README modification

--- week-01/day-05/draw-diamond.ts
+++ week-01/day-05/draw-diamond.ts
@@ -2,19 +2,6 @@
 
 let lineCount: number = 7;
 
-// Write a program that draws a
-// diamond like this:
-//
-//    *
-//   ***
-//  *****
-// *******
-//  *****
-//   ***
-//    *
-//
-// The diamond should have as many lines as lineCount is
-
 if (lineCount % 2 == 0) {
     let half: number = lineCount / 2;
     for (let i: number = 0; i < half; i ++) {
@@ -34,9 +21,9 @@ if (lineCount % 2 == 0) {
         let numOfSpaces: number = half + i - numOfStars;
         console.log(Array(numOfSpaces + 1).join(' ') + Array(numOfStars + 1).join('*'));
     }
-    for (let i: number = 0; i < half; i ++) {
-        let numOfStars: number = lineCount - (i + 1) * 2;
-        let numOfSpaces: number = i + 1;
+    for (let i: number = 1; i < half; i ++) {
+        let numOfStars: number = lineCount - i * 2;
+        let numOfSpaces: number = i;
         console.log(Array(numOfSpaces + 1).join(' ') + Array(numOfStars + 1).join('*'));
     }
 }
