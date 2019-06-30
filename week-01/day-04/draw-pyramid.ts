c4436e28207a55babd1c17697bc0f44550d6386b
Nora Pete <norapete19@gmail.com>
2019-06-30 17:41:54 +0200

    draw triangle and pyramid task added, pyramid function added

--- week-01/day-04/draw-pyramid.ts
+++ week-01/day-04/draw-pyramid.ts
@@ -1,9 +1,31 @@
 'use strict';
 
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
+
+
+function makesStringWithStars(spaces: number, stars: number) : string {
+    let stringWithStars: string = '';
+    for (let i: number = 0; i < spaces; i ++) {
+        stringWithStars += ' ';
+    }
+    for (let j: number = 0; j < stars; j ++) {
+        stringWithStars += '*';
+    }
+    return stringWithStars
+}
+
 let lineCount: number = 4;
 
-for (let i: number = 0; i < lineCount; i ++) {
-    let numOfStars: number = 2 * i + 1;
-    let numOfSpaces: number = lineCount + i - numOfStars;
-    console.log(Array(numOfSpaces + 1).join(' ') + Array(numOfStars + 1).join('*'));
+for (let k: number = 0; k < lineCount; k ++) {
+    let numOfStars: number = 2 * k + 1;
+    let numOfSpaces: number = lineCount + k - numOfStars;
+    console.log(makesStringWithStars(numOfSpaces, numOfStars));
 }
