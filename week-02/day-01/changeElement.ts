d8d7235a04e5cccd06b7cdd9140214f26b9b3015
Nora Pete <norapete19@gmail.com>
2019-07-01 13:47:55 +0200

    workshop w2d1 changeElements completed

---
+++ week-02/day-01/changeElement.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+let numList: number[] = [1, 2, 3, 8, 5, 6];
+
+let correctNumList: number[] = numList.map(function(item) {
+    if (item === 8) {
+        return 4;
+    } else {
+        return item;
+    }
+})
+
+console.log(correctNumList[3]);
