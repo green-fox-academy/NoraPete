fb877149f0b124610d643c92cfd1bc1b0aa21de9
Nora Pete <norapete19@gmail.com>
2019-07-01 17:25:01 +0200

    workshop w2d1 unique completed

---
+++ week-02/day-01/unique.ts
@@ -0,0 +1,18 @@
+'use stirct';
+
+//  Create a function that takes a list of numbers as a parameter
+//  Returns a list of numbers where every number in the list occurs only once
+
+function unique(arr: number[]) : number[] {
+    let uniqueNumbers: number[] = [];
+    arr.forEach(function (item) {
+        if (uniqueNumbers.indexOf(item) < 0)
+        uniqueNumbers.push(item);
+    })
+    return uniqueNumbers;
+
+}
+
+//  Example
+console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]))
+//  should print: `[1, 11, 34, 52, 61]`
