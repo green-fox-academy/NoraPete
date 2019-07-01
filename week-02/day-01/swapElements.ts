0522b9578485e5cc88853275f99f1451bdc175e7
Nora Pete <norapete19@gmail.com>
2019-07-01 13:34:28 +0200

    workshop w2d1 swapElements completed

---
+++ week-02/day-01/swapElements.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc: string[] = ['Arthur', 'Boe', 'Chloe'];
+abc.push(abc[0]);
+abc.splice(0, 1, abc[2]);
+abc.splice(2, 1);
+
+console.log(abc);
