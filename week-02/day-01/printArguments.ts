f792829cb39303073c90efbe898a3e71b6702a42
Nora Pete <norapete19@gmail.com>
2019-07-01 16:34:09 +0200

    workshop w2d1 printAugments completed

---
+++ week-02/day-01/printArguments.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams (...inputs: any[]) : any {
+    inputs.forEach(function (item) {
+        console.log(item);
+    })
+}
+
+printParams('Viktor', 4, true);
