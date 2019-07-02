1fc8db57d3c45d7a28a53c5323f6ad4bae35dc40
Nora Pete <norapete19@gmail.com>
2019-07-02 15:41:57 +0200

    workshop w2d2 takesLonger modified

--- week-02/day-02/takesLonger.ts
+++ week-02/day-02/takesLonger.ts
@@ -5,7 +5,7 @@
 
 let quote: string = `Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.`;
 
-function addsStringAtDefinedPosition (inputStr: string, strToAdd: string, endOfPrefix: string, startOfSuffix: string): string {
+function addsStringAtDefinedPosition(inputStr: string, strToAdd: string, endOfPrefix: string, startOfSuffix: string): string {
     let prefix: string = inputStr.slice(0, inputStr.indexOf(endOfPrefix) + endOfPrefix.length + 1);
     let suffix: string = inputStr.slice(inputStr.indexOf(startOfSuffix));
     let finalString: string = prefix.concat(' ', strToAdd, ' ', suffix);
