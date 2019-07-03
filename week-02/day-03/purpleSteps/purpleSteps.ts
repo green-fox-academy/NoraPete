a5a8e0fa973f6b634dd53bb50995e830826e0eb3
Nora Pete <norapete19@gmail.com>
2019-07-03 15:31:24 +0200

    workshop w2d3 purpleSteps completed

---
+++ week-02/day-03/purpleSteps/purpleSteps.ts
@@ -0,0 +1,31 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r3.png]
+export = {}
+
+let sqrSize: number = 10;
+let fillColor: string = 'purple';
+let strokeColor: string = 'black';
+
+function fillsSquares (x: number, y: number, size: number, col: string) {
+    ctx.fillStyle = col;
+    ctx.fillRect(x, y, size, size);
+}
+
+function strokesSquares (x: number, y: number, size: number, col: string) {
+    ctx.strokeStyle = col;
+    ctx.strokeRect(x, y, size, size);
+}
+
+for (let i: number = 0; i < 20; i ++) {
+    let xCoo: number = i * sqrSize;
+    let yCoo: number = i * sqrSize;
+    fillsSquares(xCoo, yCoo, sqrSize, fillColor)
+    strokesSquares(xCoo, yCoo, sqrSize, strokeColor)
+}
