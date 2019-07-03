12d4a6b6e579542812e1ee57ba06f30d8b5f0d9d
Nora Pete <norapete19@gmail.com>
2019-07-03 16:01:07 +0200

    workshop w2d3 purpleSteps3d modified

---
+++ week-02/day-03/purpleSteps3d/purpleSteps3d.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r4.png]
+export = {};
+
+let xCoo: number = 0;
+let yCoo: number = 0;
+
+function drawsPurpleBoxesWithBlackBorder (x: number, y: number, size: number) {
+    ctx.fillStyle = 'purple';
+    ctx.fillRect(x, y, size, size);
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(x, y, size, size);
+}
+
+for (let i: number = 0; i < 6; i ++) {
+    let sqrSize: number = 20 + i * 20
+    drawsPurpleBoxesWithBlackBorder(xCoo, yCoo, sqrSize);
+    xCoo += sqrSize;
+    yCoo += sqrSize;
+}
