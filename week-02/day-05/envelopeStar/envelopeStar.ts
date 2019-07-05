0e2a5bcc1ce894f1af6f9e7596010ac325332be6
Nora Pete <norapete19@gmail.com>
2019-07-05 14:51:08 +0200

    workshop w2d5 envelopeStar cleaned up

--- week-02/day-05/envelopeStar/envelopeStar.ts
+++ week-02/day-05/envelopeStar/envelopeStar.ts
@@ -5,33 +5,29 @@ const ctx = canvas.getContext('2d');
 
 export = {};
 
-let div: number = 2;
-let edgeX: number = 0 + canvas.width / div;
-let edgeY: number = 0 + canvas.height / div;
+let step: number = 16;
+let diff: number = canvas.width / (step * 2);
 
-let diff: number = edgeY / 16;
 ctx.beginPath();
 ctx.strokeStyle = 'limegreen';
-for (let i: number = 0; i < 16; i ++) {
-    ctx.moveTo(edgeX, 0 + i * diff);
-    ctx.lineTo(edgeX + (i + 1) * diff, edgeY);
-    ctx.stroke();
+for (let i: number = 0; i < step * 2; i++) {
+    ctx.moveTo(0 + i * diff, canvas.height / 2);
+    if (i <= step) {
+        ctx.lineTo(canvas.width / 2, canvas.height / 2 - (i + 1) * diff);
+        ctx.stroke();
+    } else {
+        ctx.lineTo(canvas.width / 2, (i - step) * diff);
+        ctx.stroke();
+    }
 }
 
-for (let i: number = 0; i < 16; i ++) {
-    ctx.moveTo(0 + i * diff, edgeY);
-    ctx.lineTo(edgeX, edgeY + (i + 1) * diff);
-    ctx.stroke();
-}
-
-for (let i: number = 0; i < 16; i ++) {
-    ctx.moveTo(edgeX, 0 + i * diff);
-    ctx.lineTo(edgeX - (i + 1) * diff, edgeY);
-    ctx.stroke();
-}
-
-for (let i: number = 0; i < 15; i ++) {
-    ctx.moveTo(edgeX, canvas.height - (i + 1) * diff);
-    ctx.lineTo(edgeX + (i + 1) * diff, edgeY);
-    ctx.stroke();
+for (let i: number = 0; i < step * 2; i++) {
+    ctx.moveTo(0 + i * diff, canvas.height / 2);
+    if (i <= step) {
+        ctx.lineTo(canvas.width / 2, canvas.height / 2 + (i + 1) * diff);
+        ctx.stroke();
+    } else {
+        ctx.lineTo(canvas.width / 2, canvas.height - (i - step) * diff);
+        ctx.stroke();
+    }
 }
