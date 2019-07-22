48eae0e480dd138f7501433ba38018517c26752f
Nora Pete <norapete19@gmail.com>
2019-07-22 21:41:11 +0200

    draw-chess-table works

--- week-01/day-05/draw-chess-table.ts
+++ week-01/day-05/draw-chess-table.ts
@@ -13,14 +13,14 @@
 //
 
 function drawsChessTable(): void {
-    for (let i: number = 0; i < 8; i ++) {
+    for (let i: number = 0; i < 8; i++) {
         let lineToPrint: string = '';
-        if(i % 2 === 0) {
-            for (let j: number = 0; j < 4; j ++) {
+        if (i % 2 === 0) {
+            for (let j: number = 0; j < 4; j++) {
                 lineToPrint += ' %';
             }
         } else {
-            for (let k: number = 0; k < 4; k ++) {
+            for (let k: number = 0; k < 4; k++) {
                 lineToPrint += '% ';
             }
         }
