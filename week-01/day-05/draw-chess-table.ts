1979e90b8c4f9a55163d1553b01f038df3d7820f
Nora Pete <norapete19@gmail.com>
2019-07-22 21:40:33 +0200

    draw-chess-table works

---
+++ week-01/day-05/draw-chess-table.ts
@@ -0,0 +1,31 @@
+'use strict';
+
+// Create a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
+
+function drawsChessTable(): void {
+    for (let i: number = 0; i < 8; i ++) {
+        let lineToPrint: string = '';
+        if(i % 2 === 0) {
+            for (let j: number = 0; j < 4; j ++) {
+                lineToPrint += ' %';
+            }
+        } else {
+            for (let k: number = 0; k < 4; k ++) {
+                lineToPrint += '% ';
+            }
+        }
+        console.log(lineToPrint);
+    }
+}
+
+drawsChessTable();
