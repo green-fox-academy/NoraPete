6ecc1ff4827edeea4ff9ecaec58c9722dec074a8
Nora Pete <norapete19@gmail.com>
2019-07-24 13:24:27 +0200

    w2 datastructures old exercises element finder compl.

---
+++ week-02/day-02/elementFinder.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+// Write a function that checks if the array contains "7"
+// if it contains return "Hoorray" otherwise return "Noooooo"
+
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+
+function containsSeven(list: number[]): string {
+    let outputString: string = 'Nooooooo';
+    if (list.some(function (num) {
+        return (num === 7);
+    })) {
+        outputString = 'Hoorray';
+    }
+    return outputString;
+}
+
+console.log(containsSeven(numbers));
+// The output should be: "Noooooo"
+// Do this again with a different solution using different list functions!
+
+export = containsSeven;
