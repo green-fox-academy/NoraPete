c892b736c08c3dc2264c8fa13fecdaf8f667a520
Nora Pete <norapete19@gmail.com>
2019-07-26 13:23:10 +0200

    w3 fileIO reversedLines compl.

---
+++ week-03/day-02/reversedLines/reversedLines.ts
@@ -0,0 +1,42 @@
+'use strict';
+
+declare function require(path: string): any;
+
+const fs = require('fs');
+
+// Create a method that decrypts reversed-lines.txt
+
+function readsFile(path: string): string {
+    if (!fs.existsSync(path)) {
+        throw Error('File does not exist');
+    }
+    return fs.readFileSync(path, 'utf-8');
+}
+
+function getsSingleLinesAsAMatrix(inputStr: string): string[][] {
+    let lines: string[] = inputStr.split('\r\n');
+    let matrix: string[][] = [];
+    for (let i: number = 0; i < lines.length; i ++) {
+        matrix.push(lines[i].split(''));
+    }
+    return matrix;
+}
+
+function correctsReversedLines(path: string): void {
+    let fileContent: string;
+    try {
+        fileContent = readsFile(path);
+    } catch (e) {
+        console.log(e);
+        fileContent = '';
+    }
+    let contentMatrix: string[][] = getsSingleLinesAsAMatrix(fileContent);
+    let correctLines: string[] = [];
+    for (let i: number = 0; i < contentMatrix.length; i ++) {
+        correctLines.push(contentMatrix[i].reverse().join(''));
+    }
+    let correctText: string = correctLines.join('\r\n');
+    fs.writeFileSync(path, correctText);
+}
+
+correctsReversedLines('./reversedLines.txt');
