76b1a38b0ed89f771d53f371341373a6a7bb316d
Nora Pete <norapete19@gmail.com>
2019-07-28 17:50:13 +0200

    w3 recursion strings compl.

---
+++ week-03/day-05/strings.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+function changesXToY(inputStr: string): string {
+    if (inputStr.indexOf('x') < 0) {
+        return inputStr;
+    } else {
+        return  changesXToY(inputStr.replace('x', 'y'));
+    }
+}
+
+console.log(changesXToY('axolotl'));
