6597684de0e95b327288d9453e7c6820ffaf3144
Nora Pete <norapete19@gmail.com>
2019-08-07 12:37:15 +0200

    add(helloWorld):w6d2 set up server and change template

---
+++ week-06/day-03/helloWorld/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home', {title: 'Hello World!',})
+})
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+})
