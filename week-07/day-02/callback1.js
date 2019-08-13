922f4160493a92f9ad4d214f70465a31850c0774
Nora Pete <norapete19@gmail.com>
2019-08-13 12:20:36 +0200

    w7d2(simpleTimeout): add timer function

--- week-07/day-02/callback1.js
+++ week-07/day-02/callback1.js
@@ -2,14 +2,14 @@
 
 const mapWith = (array, callback) => {
   let output = [];
-  for (let i = 0; i < array.length; i ++) {
+  for (let i = 0; i < array.length; i++) {
     output.push(callback(array[i]));
   }
   return output;
 }
-  // The mapWith() function should iterate over the given array and call the callback function on every element.
-  // It stores the callback return values in the output.
-  // The mapWith() should return with the output array.
+// The mapWith() function should iterate over the given array and call the callback function on every element.
+// It stores the callback return values in the output.
+// The mapWith() should return with the output array.
 
 
 const addOne = (number) => {
@@ -25,9 +25,9 @@ console.log(mapWith([1, 2, 3], addOne));
 
 // Create a callback function which removes every second character from a string
 
-const removeSecondLetter = function(word) {
+const removeSecondLetter = function (word) {
   let letters = word.split('');
-  return letters.filter(function(letter) {
+  return letters.filter(function (letter) {
     return letters.indexOf(letter) % 2 === 0;
   }).join('');
 }
