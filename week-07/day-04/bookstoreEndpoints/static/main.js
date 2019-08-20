2bbb7108fec00d6a966a2e89a21503b9a1d37349
Nora Pete <norapete19@gmail.com>
2019-08-20 08:43:41 +0200

    refactor(bookstore)

--- week-07/day-04/bookstoreEndpoints/static/main.js
+++ week-07/day-04/bookstoreEndpoints/static/main.js
@@ -3,9 +3,9 @@
 
 function sendGetRequest(url, callback) {
   const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE) {
-      if(xhr.status === 200) {
+      if (xhr.status === 200) {
         callback(JSON.parse(xhr.responseText));
       } else {
         console.log('something went wrong');
@@ -19,27 +19,26 @@ function sendGetRequest(url, callback) {
 function renderSearchResults(response) {
   let listOfBooks = document.createElement('ul');
   response.forEach(function (book) {
-    let listElement = document.createElement('li');
-    let title = document.createElement('p');
-    title.innerText = book.book_name;
-    let author = document.createElement('p');
-    author.innerText= book.aut_name;
-    let category = document.createElement('p');
-    category.innerText = book.cate_descrip;
-    let publisher = document.createElement('p');
-    publisher.innerText = book.pub_name;
-    let price = document.createElement('p');
-    price.innerText = book.book_price;
-    listElement.appendChild(title);
-    listElement.appendChild(author);
-    listElement.appendChild(category);
-    listElement.appendChild(publisher);
-    listElement.appendChild(price);
-    listOfBooks.appendChild(listElement);
+    listOfBooks.appendChild(collectBookInfo(book));
   });
   document.querySelector('main').appendChild(listOfBooks);
 }
 
+function collectBookInfo(book) {
+  let listElement = document.createElement('li');
+  listElement.appendChild(getBookInfo(book, 'book_name'));
+  listElement.appendChild(getBookInfo(book, 'aut_name'));
+  listElement.appendChild(getBookInfo(book, 'cate_descrip'));
+  listElement.appendChild(getBookInfo(book, 'pub_name'));
+  listElement.appendChild(getBookInfo(book, 'book_price'));
+  return listElement;
+}
+
+function getBookInfo(book, infoType) {
+  let info = document.createElement('p');
+  info.innerText = book[infoType];
+  return info;
+}
 
 window.onload = () => {
   sendGetRequest('http://localhost:3000/books', renderSearchResults);
