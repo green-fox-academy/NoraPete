1b9d121df96b64d73aa072a6b1c902793230cbba
Nora Pete <norapete19@gmail.com>
2019-08-22 11:54:36 +0200

    add(w8d4counter): add js to html

---
+++ week-08/day-04/counter/events-2.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const button = document.querySelector('button');
+const result = document.querySelector('.result');
+const liTags = document.querySelectorAll('li');
+
+function countElements(list) {
+  return list.length;
+}
+
+button.addEventListener('click', function() {
+  result.innerHTML = countElements(liTags);
+})
