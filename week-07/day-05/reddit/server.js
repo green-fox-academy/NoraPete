89c83b0a15df42eb15944090bb2670fd381e0c1d
Nora Pete <norapete19@gmail.com>
2019-08-28 17:28:49 +0200

    feat(w7d4redditBackend): add /posts endpoint

--- week-07/day-05/reddit/server.js
+++ week-07/day-05/reddit/server.js
@@ -13,6 +13,8 @@ const connection = mysql.createConnection({
   database: 'reddit'
 });
 
+app.use(express.json());
+
 connection.connect(function (err) {
   if (err) {
     console.log(err);
@@ -23,6 +25,18 @@ connection.connect(function (err) {
 
 app.get('/hello', function(req, res) {
   res.send('Hello World!');
+});
+
+app.get('/posts', function(req, res) {
+  connection.query('SELECT * FROM posts;', (error, result) => {
+    if(error)  {
+      console.log('Couldnt get data from the database', error);
+      return;
+    }
+    res.status(200);
+    res.setHeader('Content-Type', 'application/json');
+    res.send({ posts: result });
+  });
 });
 
 app.listen(PORT, function() {
