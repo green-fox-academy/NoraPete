3de302abe3f8a2998c0f078161928ba0b8657169
Nora Pete <norapete19@gmail.com>
2019-09-07 16:09:46 +0200

    feat(w9d4redditFrontend): add voting functionality

--- week-09/day-04/redditFrontend/assets/js/main.js
+++ week-09/day-04/redditFrontend/assets/js/main.js
@@ -2,8 +2,52 @@
 
 let mainContent = document.querySelector('main');
 
+function checkResponseStatus(res) {
+  if (res.status < 200 || res.status >= 300) {
+    return new Error('Something went wrong');
+  }
+  return res;
+}
+
+function getId(event) {
+  let voteDiv = event.target.parentNode;
+  return voteDiv.parentNode.getAttribute('id').slice(1);
+}
+
+function getUpOrDown(event) {
+  return event.target.getAttribute('alt');
+}
+
+function updateScore(id, score) {
+  let displayedScore = document.querySelector(`#p${id} > div > p`);
+  displayedScore.innerText = score;
+}
+
+function sendRequestToVote(upOrDown, id) {
+  return fetch(`http://secure-reddit.herokuapp.com/simple/posts/${id}/${upOrDown}`, {
+    method: 'PUT',
+    headers: { 'Accept': 'application/json' }
+  })
+    .then(checkResponseStatus)
+    .then(response => response.json())
+    .then(json => updateScore(id, json.score))
+    .catch(console.log)
+}
+
 fetch('/reddit')
   .then(res => res.text())
   .then(content => mainContent.innerHTML = content)
+  .then(() => {
+    let voteBlock = document.querySelectorAll('.vote');
+    voteBlock.forEach((block) => {
+      block.addEventListener('click', (e) => {
+        if(getUpOrDown(e) === 'upvote') {
+          e.target.setAttribute('src', 'images/upvoted.png');
+        } else {
+          e.target.setAttribute('src', 'images/downvoted.png');
+        }
+        sendRequestToVote(getUpOrDown(e), getId(e));
+      })
+    })
+  })
   .catch(console.log);
-  
