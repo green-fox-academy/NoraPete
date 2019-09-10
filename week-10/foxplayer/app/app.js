511bdcfe2e31ff6579f3c56e869fed6f3f9c02dd
Nora Pete <norapete19@gmail.com>
2019-09-10 14:52:01 +0200

    add(w10foxplayer): add module to map a given directory for music files

--- week-10/foxplayer/app/app.js
+++ week-10/foxplayer/app/app.js
@@ -14,7 +14,7 @@ const connection = mysql.createConnection({
 app.use(express.static('static'));
 app.use(express.json());
 
-connection.connect(function(dbConnectionError) {
+connection.connect(function (dbConnectionError) {
   if (dbConnectionError) {
     console.log(dbConnectionError);
     return;
@@ -22,13 +22,13 @@ connection.connect(function(dbConnectionError) {
   console.log('Connected to the database');
 })
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.resolve('static/experiment/experiment.html'));
 });
 
-app.get('/playlists', function(req, res) {
-  connection.query('SELECT * FROM playlists', function(err, rows) {
-    if(err) {
+app.get('/playlists', function (req, res) {
+  connection.query('SELECT * FROM playlists', function (err, rows) {
+    if (err) {
       console.log('Could not get data from database');
       return;
     }
@@ -36,9 +36,9 @@ app.get('/playlists', function(req, res) {
   });
 });
 
-app.post('/playlists', function(req, res) {
-  connection.query('INSERT INTO playlists SET ?', req.body, function(err, rows) {
-    if(err) {
+app.post('/playlists', function (req, res) {
+  connection.query('INSERT INTO playlists SET ?', req.body, function (err, rows) {
+    if (err) {
       console.log('Could not insert playlist into database', err);
       return;
     }
@@ -46,28 +46,24 @@ app.post('/playlists', function(req, res) {
   });
 });
 
-app.delete('/playlists/:id', function(req, res) {
-connection.query('SELECT sys FROM playlists WHERE id = ?', req.params.id, function(err, rows) {
-  if(err) {
-    console.log('Could not find the requested playlist');
-    return;
-  }
-  if(rows[0].sys === 0) {
-    connection.query('DELETE FROM playlists WHERE id = ?', req.params.id, function(delErr, rows) {
-      if(delErr) {
-        console.log('Could not delete playlist from database');
-        return;
-      }
-      res.send('ok');
-    });
-  } else {
-    res.send('You cannot delete this playlist');
-  }
-});
-
-/*
-  
-  */
+app.delete('/playlists/:id', function (req, res) {
+  connection.query('SELECT sys FROM playlists WHERE id = ?', req.params.id, function (err, rows) {
+    if (err) {
+      console.log('Could not find the requested playlist');
+      return;
+    }
+    if (rows[0].sys === 0) {
+      connection.query('DELETE FROM playlists WHERE id = ?', req.params.id, function (delErr, rows) {
+        if (delErr) {
+          console.log('Could not delete playlist from database');
+          return;
+        }
+        res.send('ok');
+      });
+    } else {
+      res.send('You cannot delete this playlist');
+    }
+  });
 });
 
 module.exports = app;
---
+++ week-10/foxplayer/app/mapFolder.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+function mapDirectory(directory) {
+  let fileArray = [];
+  let reFilename = /\.\w+$/;
+  let folderContent = fs.readdirSync(directory);
+  folderContent.forEach(function(location) {
+    if(location.search(reFilename) > -1) {
+      fileArray.push(`${directory}/${location}`);
+    } else {
+      mapDirectory(`${directory}/${location}`).forEach(function (file) {
+        fileArray.push(file);
+      });
+    }
+  });
+  return fileArray;
+}
+
+function getMusicFilePaths(directory) {
+  let tracks = mapDirectory(directory).filter(function(file) {
+    return file.search('.mp3') > -1;
+  });
+  return tracks.map(function(track) {
+    return path.resolve(track);
+  });
+}
+
+module.exports = getMusicFilePaths;
